2539f1544c27444e3830501e3dc22ed1f3b5d104
Aaron Paden <aaronbpaden@gmail.com>
2021-12-07 11:29:28 -0600

    Add images to media folder and create db object in app.js

---
+++ js/app.js
@@ -0,0 +1,45 @@
+"use strict";
+const db = [
+    {
+        id: 0,
+        name: "Blue and Green Bag",
+        price: 129.99,
+        img: "media/blue-and-green-bag.jpg",
+        featured: true,
+    },
+    {
+        id: 1,
+        name: "Barbour Loden Coat",
+        price: 129.99,
+        img: "media/barbour-loden-coat.jpg",
+        featured: false,
+    },
+    {
+        id: 2,
+        name: "Brown Suede Shoes",
+        price: 129.99,
+        img: "media/brown-suede-shoes.jpg",
+        featured: true,
+    },
+    {
+        id: 3,
+        name: "Roy Bjorkman Fur Coat",
+        price: 129.99,
+        img: "media/roy-bjorkman-fur-coat.jpg",
+        featured: false,
+    },
+    {
+        id: 4,
+        name: "Steeplechase Hat",
+        price: 129.99,
+        img: "media/steeplechase-hat.jpg",
+        featured: true,
+    },
+    {
+        id: 5,
+        name: "Shoes",
+        price: 129.99,
+        img: "media/shoes.jpg",
+        featured: false,
+    },
+];
