6befde1da8b4cebf9620c00a9ba8edba2b4a9c20
Aaron Paden <aaronbpaden@gmail.com>
2021-12-07 11:52:42 -0600

    populate fake slider.

--- js/app.js
+++ js/app.js
@@ -3,43 +3,88 @@ const db = [
     {
         id: 0,
         name: "Blue and Green Bag",
+        desc: "It's a store item",
         price: 129.99,
         img: "media/blue-and-green-bag.jpg",
-        featured: true,
+        top: true,
     },
     {
         id: 1,
         name: "Barbour Loden Coat",
+        desc: "It's a store item",
         price: 129.99,
         img: "media/barbour-loden-coat.jpg",
-        featured: false,
+        top: false,
     },
     {
         id: 2,
         name: "Brown Suede Shoes",
+        desc: "It's a store item",
         price: 129.99,
         img: "media/brown-suede-shoes.jpg",
-        featured: true,
+        top: true,
     },
     {
         id: 3,
         name: "Roy Bjorkman Fur Coat",
+        desc: "It's a store item",
         price: 129.99,
         img: "media/roy-bjorkman-fur-coat.jpg",
-        featured: false,
+        top: false,
     },
     {
         id: 4,
         name: "Steeplechase Hat",
+        desc: "It's a store item",
         price: 129.99,
         img: "media/steeplechase-hat.jpg",
-        featured: true,
+        top: true,
     },
     {
         id: 5,
         name: "Shoes",
+        desc: "It's a store item",
         price: 129.99,
         img: "media/shoes.jpg",
-        featured: false,
+        top: false,
     },
 ];
+
+class ShoppingPage {
+    constructor() {
+        this.sliderBox = document.getElementById("sliderBox");
+        this.topBox = document.getElementById("topBox");
+        this.topItems = db.filter(e => e.top);
+        this.featuredBox = document.getElementById("featuredBox");
+        this.populate_slider();
+        this.populate_top();
+        this.populate_featured();
+    }
+
+    populate_slider() {
+        let item = this.topItems[0];
+        document.getElementById("sliderFigure").innerHTML = `
+            <img src="${item.img}" alt="${item.name}" class="img-fluid slider-img">`;
+        sliderBox.innerHTML = `
+            <h2 class="slider-title">${item.name}</h2>
+            <p class="slider-text">${item.desc}</p>
+            <button class="slider-button">shop now</button>
+            <ul class="slider-button-list">
+                <li class="slider-button-item"><input type="radio" class="slider-radio" name="slider" value="" checked></li>
+                <li class="slider-button-item"><input type="radio" class="slider-radio" name="slider" value=""></li>
+                <li class="slider-button-item"><input type="radio" class="slider-radio" name="slider" value=""></li>
+                <li class="slider-button-item"><input type="radio" class="slider-radio" name="slider" value=""></li>
+                <li class="slider-button-item"><input type="radio" class="slider-radio" name="slider" value=""></li>
+            </ul>`;
+    }
+
+    populate_top() {
+        return;
+    }
+
+    populate_featured() {
+        return;
+    }
+}
+
+new ShoppingPage();
